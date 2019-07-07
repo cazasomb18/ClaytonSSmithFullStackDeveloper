b1c18506d88601685e0baf624500e6d6ad6664b0
claytonschoonoversmith@gmail.com <claytonsmith@Claytons-MacBook-Pro.local>
2019-07-07 15:50:09 -0500

    added boostrap 4.3.1 carousel, responsivity

--- ClaytonSSmithFullStackDeveloper/js/app.js
+++ ClaytonSSmithFullStackDeveloper/js/app.js
@@ -1,1 +1,9 @@
-console.log('CLAYTON S. SMITH\'S PORTFOLIO SITE');
+console.log("CLAYTON S. SMITH\'S PORTFOLIO SITE");
+
+/// let's create some listeners for clicks on buttons that will direct you to parts of the site///
+/// obviously we'll want to make the listeners for clicks happen on the NAVBAR links ///
+
+///RESEARCH:  how to scroll down to a portion of a screen when a link is clicked///
+
+/// ANSWER: <a href="#idofelement"><a/> <div id="idofelement"></div> ///
+/// NOTE: this does not create a "smooth scroll" ///
